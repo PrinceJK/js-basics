87c842919c95f8737a1eb640c940019d3d277241
John Kehinde <kehindejohn018@gmail.com>
2022-02-21 15:04:51 +0100

    Functions

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-let selectedColors = ['red', 'blue'];
-selectedColors[2] = 1;
-console.log(selectedColors.length );
+//performing a task
+function greet(name, lastName){
+    console.log('Hello ' + name +' ' + lastName);
+}
+
+//Calculate a value
+function square(number){
+    return number * number;
+}
+
+
+
+console.log(square(2));
