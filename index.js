9e07912fa4231bf9616ecf6ebe5edf94bbb70193
John Kehinde <jkehinde@axxess.com>
2022-02-27 23:41:12 +0100

    Algorithm

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 //performing a task
-function greet(name, lastName){
-    console.log('Hello ' + name +' ' + lastName);
+// function greet(name, lastName){
+//     console.log('Hello ' + name +' ' + lastName);
+// }
+
+// //Calculate a value
+// function square(number){
+//     return number * number;
+// }
+//let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function findMissing(arrOfNumbers){
+    let sumOfArray = 0;
+    if(isNaN(arrOfNumbers)){
+        return NaN;
+    }
+    for(let i = 0; i < arrOfNumbers.length; i++){
+        sumOfArray += arrOfNumbers[i];
+        continue;
+    }
+    if(sumOfArray === 45){
+        return -1
+    }
+    return 45 - sumOfArray;
 }
 
-//Calculate a value
-function square(number){
-    return number * number;
-}
 
 
 
